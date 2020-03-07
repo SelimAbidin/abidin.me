ead23f1882ac82fa931e1913fd43a1e23c8359e9
SelimAbidin <ysabidin@gmail.com>
2020-03-07 23:38:56 +0300

    website test

--- website/src/App.test.js
+++ website/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import {App} from './App.jsx';
 
-test('renders learn react link', () => {
+test('renders <App />', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Y. Selim/i);
   expect(linkElement).toBeInTheDocument();
 });
